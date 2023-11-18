572ec93fc230bc484a79b387f9a0d6c22efeb95a
Benson Tamunoemi <bensontamunoemi@gmail.com>
2023-11-18 03:23:31 -0500

    created a snake class & moved to OOP

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,27 +8,12 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-
-segment = []
-
-for position in starting_position:
-    new_segment = Turtle()
-    new_segment.shape("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segment.append(new_segment)
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg_num in range(len(segment) - 1, 0, -1):
-        new_x = segment[seg_num - 1].xcor()
-        new_y = segment[seg_num - 1].ycor()
-        segment[seg_num].goto(new_x, new_y)
-    segment[0].forward(20)
-    segment[0].left(90)
+    snake.move()
 
 screen.exitonclick()
