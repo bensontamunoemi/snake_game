203fbcce258bc18c72d0a36ade76185bb1ed879c
Benson Tamunoemi <bensontamunoemi@gmail.com>
2023-11-18 03:06:03 -0500

    animating the snake segments on screen

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 starting_position = [(0, 0), (-20, 0), (-40, 0)]
+
+segment = []
 
 for position in starting_position:
     new_segment = Turtle()
     new_segment.shape("square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segment.append(new_segment)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg_num in range(len(segment) - 1, 0, -1):
+        new_x = segment[seg_num - 1].xcor()
+        new_y = segment[seg_num - 1].ycor()
+        segment[seg_num].goto(new_x, new_y)
+    segment[0].forward(20)
+    segment[0].left(90)
 
 screen.exitonclick()
