d0e4c047b9dad59d4d2046fe423ecc9f001569cc
Benson Tamunoemi <bensontamunoemi@gmail.com>
2023-11-18 23:24:08 -0500

    detect coalition with tail

--- snake.py
+++ snake.py
@@ -10,24 +10,30 @@ RIGHT = 0
 
 class Snake:
     def __init__(self):
-        self.segment = []
+        self.segments = []
         self.create_snake()
-        self.head = self.segment[0]
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITION:
-            new_segment = Turtle()
-            new_segment.shape("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segment.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle()
+        new_segment.shape("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
-        for seg_num in range(len(self.segment) - 1, 0, -1):
-            new_x = self.segment[seg_num - 1].xcor()
-            new_y = self.segment[seg_num - 1].ycor()
-            self.segment[seg_num].goto(new_x, new_y)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
